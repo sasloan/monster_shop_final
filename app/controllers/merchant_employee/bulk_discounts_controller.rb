d486d364e966efb080db49a406f8b4b17995ab59
sasloan <sebastiansloan94@gmail.com>
2020-03-01 08:57:27 -0700

    Create show page for bulk_discounts

--- app/controllers/merchant_employee/bulk_discounts_controller.rb
+++ app/controllers/merchant_employee/bulk_discounts_controller.rb
@@ -2,6 +2,11 @@ class MerchantEmployee::BulkDiscountsController < MerchantEmployee::BaseControll
 
 	def index
 		@merchant = Merchant.find(params[:id])
-		@bulk_discounts = @merchant.bulk_discounts.all 
+		@bulk_discounts = @merchant.bulk_discounts.all
+	end
+
+	def show
+		@bulk_discount = BulkDiscount.find(params[:id])
+		@merchant = @bulk_discount.merchant 
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,7 @@ Rails.application.routes.draw do
     delete '/merchants/:id/items/:id', to: 'items#destroy'
     patch '/orders/:id', to: 'item_orders#update'
 		get '/merchants/:id/bulk_discounts', to: 'bulk_discounts#index'
+		get '/merchants/:id/bulk_discounts/:id', to: 'bulk_discounts#show'
   end
 
 	# Admin
